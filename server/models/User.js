cc8943fefe87e8aa853420cf00b2361986f7f8ee
Anees <Aneesdropz@gmail.com>
2025-11-15 14:16:47 +0530

    Created User.js file and setup  userSchema and in that

---
+++ server/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:  [true, 'Name is required'],
+        trim: true,
+        minlength: [2, 'Name must be at least 2 characters long'],
+        maxlength: [50, 'Name cannot exceed 50 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+        maxlength: [20, 'Password cant exceed 20 characters']
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    status: {
+        type: String,
+        enum: ['active', 'banned'],
+        default: 'active',
+    },
+    banReason: {
+        type: String,
+        default: null
+    },
+    bannedAt: {
+        type: Date,
+        default: null
+    },
+    bannedBy: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        default: null
+    },
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model('User',userSchema)
